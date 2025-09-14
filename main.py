7662115a169ce6776aef1c3638a47ea9d8141417
mikweme <167899547+mikweme@users.noreply.github.com>
2025-09-14 12:47:45 +0300

    setting up player and drawing on screen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from constants import *
+from player import Player
 import pygame
 
 # this allows us to use code from
@@ -11,21 +12,32 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # Pygame setup
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids")
-    game_clock = pygame.time.Clock()
+    game_clock = pygame.time.Clock() # Create a clock object to manage the frame rate
     dt = 0
 
+    # Player setup
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2   
+    player = Player(x, y)
+
+    # Main game loop
     running = True
     while running:
+        # Stops the game if the window is closed
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        screen.fill((0, 0, 0))  # Fill the screen with black
+        screen.fill((0, 0, 0))
+        player.draw(screen)
+        
         pygame.display.flip()   # Update the display
         dt = game_clock.tick(60) / 1000  # Limit to 60 FPS and get delta time
+
 
 if __name__ == "__main__":
     main()
